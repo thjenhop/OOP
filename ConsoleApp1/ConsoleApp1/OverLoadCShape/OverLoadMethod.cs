175d8bd16aaa277833c4ad3de84b4b1ca84dda8a
confino <thjenhop99>
2019-06-24 23:49:21 +0700

    Simple code Overriding - OverLoading - Asbtract: Method

---
+++ ConsoleApp1/ConsoleApp1/OverLoadCShape/OverLoadMethod.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1.OverLoadCShape
+{
+    class OverLoadMethod
+    {
+        public int Add(int x)
+        {
+            return 0;
+        }
+
+        // 2 method is invalid for overload method in CShape - Java
+
+
+        //int Add(int x)
+        //{
+        //    return 0;
+        //}
+        //public int Add(int x)
+        //{
+        //    return 0;
+        //}
+        public int Add(int x, int y)
+        {
+            return 0;
+        }
+        // CShape overload : same 
+        //+ Datatype return 
+        //+ MethodName
+       
+    }
+}
---
+++ ConsoleApp1/ConsoleApp1/OverWriteCShape/OverrideMethod.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1.OverWriteCShape
+{
+    interface I
+    {
+        void M();
+    }
+
+    abstract class C : I
+    {
+        public abstract void M();
+    }
+
+    public abstract class Shape
+    {
+        public abstract int GetArea();
+    }
+    
+    public class Square : Shape
+    {
+        public int a;
+
+        public Square(int n) => a = n;
+        // funtional programing CShape
+
+        public override int GetArea() => a * a;
+
+    }
+
+}
